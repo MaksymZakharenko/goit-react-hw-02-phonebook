da42044a58e70dec402b30f25f02091764fb0b26
MaksymZakharenko <raspik83@gmail.com>
2021-06-13 04:50:27 +0300

    HW 2.2 v1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todos from "./components/Contacts";
+
+
+const App = () => {
+  return (
+    <div className="block">
+      <Todos />
+    </div>
+  );
+};
+
+export default App;
